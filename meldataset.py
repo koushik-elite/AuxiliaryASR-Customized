6c2e6fefa20327274676266c09186077e100417a
koushik <koushik_2031@yahoo.com>
2023-10-24 21:33:37 +0530

    issue fixed

--- meldataset.py
+++ meldataset.py
@@ -85,7 +85,7 @@ class MelDataset(torch.utils.data.Dataset):
         # phonemize the text
         # ps = self.g2p(text.replace('-', ' '))
         # ps = text.replace('-', ' ')
-        ps = ps.split("__")
+        ps = text.split("__")
         # print(ps)
         # if "'" in ps:
         #     ps.remove("'")
